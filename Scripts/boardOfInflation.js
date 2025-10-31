7b9182a254dea3edb4b51b0ba536c1167a9db1fc
SzaeZal <Sza3Zal@gmail.com>
2025-10-31 14:15:52 +0100

    added the usual base UI stuff to board of inflation

---
+++ Scripts/boardOfInflation.js
@@ -0,0 +1,504 @@
+$(()=>{
+    $("#goBackToHub").on("click", ()=>{
+        window.location.href="../index.html"
+    })
+    //#region player
+    let player={
+        stats:{
+            points: 1,
+            stars: 0,
+            position: 0,
+            eyeOfInfinityUnlocked: false,
+            effects:{
+                noRedSquareDivisions:{
+                    turnsLeft: 0,
+                },
+                keyOfInfinity:{
+                    turnsLeft: -1
+                },
+                lockpickKit:{
+                    turnsLeft: 0
+                },
+                rerolls:{
+                    turnsLeft: 0
+                }
+            },
+            upgrades:{
+                greenBaseDoubler:{
+                    bought: false
+                },
+                secondDice:{
+                    bought: false
+                },
+                antiDice:{
+                    bought: false
+                }
+            }
+        },
+        shadowClone:{
+            active:false,
+            position: 0
+        },
+        options:{
+            ui:{
+                theme: "Dark",
+                subMenuShown: true,
+            }
+        }
+    }
+    //#endregion
+    //#region playerStatsCalculated
+    let playerStatsCalculated={
+        //idk if I need this
+    }
+    //#endregion   
+    //#region sidebar open-close
+    let sidebar = $("#sidebar")
+    let isSidebarOpen=true
+    $("#close-open").on("click", ()=>{
+        if(isSidebarOpen){
+            sidebar.removeClass("openSidebar")
+            sidebar.addClass("closeSidebar")
+            $("#close-open").html(`
+                <svg 
+                    xmlns="http://www.w3.org/2000/svg" 
+                    height="49px" 
+                    viewBox="0 -960 960 960" 
+                    width="49px" 
+                    fill="#888">
+                        <path d="M504-480 320-664l56-56 240 240-240 240-56-56 184-184Z"/>
+                </svg>
+            `)
+        }
+        else{
+            sidebar.removeClass("closeSidebar")
+            sidebar.addClass("openSidebar")
+            $("#close-open").html(`
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="49px"
+                    viewBox="0 -960 960 960"
+                    width="49px"
+                    fill="#888">
+                        <path d="M560-240 320-480l240-240 56 56-184 184 184 184-56 56Z" />
+                </svg>
+            `)
+        }
+        isSidebarOpen=!isSidebarOpen
+    })
+    //#endregion
+    //#region notification
+    let notificationIds=[0]
+    let notificationHiders=[]
+
+    const ShowNotification=(notificationTitle, notificationText, notificationType)=>{
+        let currentId=notificationIds[notificationIds.length-1]
+        $("#notifications").append(`
+            <div class="notification notification${notificationType} interactable" id="notification${currentId}">
+                ${  notificationTitle!=""
+                    ? `
+                        <h5>${notificationTitle}</h5>
+                        <hr>
+                    ` : ""
+                }
+                ${notificationText}
+            </div>    
+        `)
+
+        
+        notificationHiders.push(setTimeout(()=>{
+            $(`#notification${currentId}`).addClass("hiddenNotification")
+        }, 4000))
+
+        $("#notification"+currentId).on("click", ()=>{
+            clearTimeout(notificationHiders[currentId])
+            $(`#notification${currentId}`).addClass("hiddenNotification")
+        })
+
+        notificationIds.push(notificationIds.length)
+    }
+    //#endregion
+    //#region dialog box
+    const ShowDialogBox=(dialogTitle, dialogText, dialogType, callbackYes, callbackNo)=>{
+        $("#dialogBox").html(`
+            <div class="dialogBoxContainer dialogBox${dialogType}">
+                <div class="dialogBoxHeader">
+                    ${dialogTitle}
+                    <div id="closeDialogBox" class="closeDialogBox interactable">&#10005;</div>
+                </div>
+                <div class="dialogBoxContent">
+                    ${dialogText}
+                </div>
+                <div class="dialogBoxButtons">
+                    ${callbackYes==undefined 
+                        ? `<div id="dialogBoxCancel" class="dialogBoxButton dialogBoxButton${dialogType} interactable">I understand</div>`
+                        : `
+                            <div id="dialogBoxCancel" class="dialogBoxButton dialogBoxButton${dialogType} interactable">Cancel</div>
+                            <div id="dialogBoxYes" class="dialogBoxButton dialogBoxButton${dialogType} interactable">Yes</div>
+                        `
+                    }
+                </div>
+            </div>
+        `)
+        $("#dialogBox").removeClass("hiddenDialogBox")
+        AddDialogBoxEvents(callbackYes, callbackNo)
+    }
+
+    const AddDialogBoxEvents=(callbackYes, callbackNo)=>{
+        $("#closeDialogBox").on("click", ()=>{
+            $("#dialogBox").addClass("hiddenDialogBox")
+        })
+        $("#dialogBoxCancel").on("click", ()=>{
+            $("#dialogBox").addClass("hiddenDialogBox")
+            if(callbackNo) callbackNo()
+        })
+        if(callbackYes==undefined) return
+        $("#dialogBoxYes").on("click", ()=>{
+            callbackYes()
+            $("#dialogBox").addClass("hiddenDialogBox")
+        })
+    }
+    //#endregion
+    //#region menu navigation
+    const view = $("#view")
+    let mainMenuIndex=2
+    let subMenuIndexes=[0, 0, 0]
+    let subMenuLimits=[0, 1, 0]
+    $(document).keydown((e)=>{  
+        if(e.originalEvent.code == "KeyS" || e.originalEvent.code == "ArrowDown"){
+            mainMenuIndex= mainMenuIndex == subMenuLimits.length-1 ? 0 : mainMenuIndex+1
+            mainMenuCallbacks[mainMenuIndex]()
+        }
+        else if(e.originalEvent.code == "KeyW" || e.originalEvent.code == "ArrowUp"){
+            mainMenuIndex= mainMenuIndex == 0 ? subMenuLimits.length-1 : mainMenuIndex-1
+            mainMenuCallbacks[mainMenuIndex]()
+        }
+        else if(e.originalEvent.code == "KeyA" || e.originalEvent.code == "ArrowLeft"){
+            subMenuIndexes[mainMenuIndex] = subMenuIndexes[mainMenuIndex] == 0 ? subMenuLimits[mainMenuIndex] : subMenuIndexes[mainMenuIndex]-1
+            mainMenuCallbacks[mainMenuIndex]()
+        }
+        else if(e.originalEvent.code == "KeyD" || e.originalEvent.code == "ArrowRight"){
+            subMenuIndexes[mainMenuIndex] = subMenuIndexes[mainMenuIndex] == subMenuLimits[mainMenuIndex] ? 0 : subMenuIndexes[mainMenuIndex]+1
+            mainMenuCallbacks[mainMenuIndex]()
+        }
+    })
+
+    //#endregion
+    //#region Settings
+    $("#options").on("click", ()=>{
+        mainMenuIndex=0
+        GoToSettings()
+    })
+
+    const GoToSettings = () =>{
+        view.html(`
+            <div id="subMenuInView" ${player.options.ui.subMenuShown==false ? 'class="subMenuHidden"' : ''}>
+                <div class="subMenuItem selectedSubMenuItem">
+                    Settings
+                </div>
+            </div>
+            <div class="mainView">
+                <div class="settings">
+                    <div class="setting">
+                        <div class="settingTitle">
+                            Theme
+                        </div>
+                        <div class="options">
+                            <div id="themeDarkOption" class="option interactable ${player.options.ui.theme=="Dark" ? "selectedOption" : ""}">
+                                Dark
+                            </div>
+                            <div id="themeLightOption" class="option interactable ${player.options.ui.theme=="Light" ? "selectedOption" : ""}">
+                                Light
+                            </div>
+                        </div>
+                    </div>
+                    <div class="setting">
+                        <div class="settingTitle">
+                            Sub menu display
+                        </div>
+                        <div class="options">
+                            <div id="subMenuDisplayShownOption" class="option interactable ${player.options.ui.subMenuShown==true ? "selectedOption" : ""}">
+                                Shown
+                            </div>
+                            <div id="subMenuDisplayHiddenOption" class="option interactable ${player.options.ui.subMenuShown==false ? "selectedOption" : ""}">
+                                Hidden
+                            </div>
+                        </div>
+                    </div>
+                    <div class="setting">
+                        <div class="settingTitle">
+                            Save Related
+                        </div>
+                        <div class="options">
+                            <div id="exportSaveToClipboard" class="option interactable">
+                                Export Save to clipboard
+                            </div>
+                            <div id="importSave" class="option interactable">
+                                Import Save
+                            </div>
+                            <div id="hardReset" class="option danger interactable">
+                                Hard Reset
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>    
+        `)
+        AddSettingsUIEvents()
+    }
+    //#endregion
+    //#region Settings Events
+    const AddSettingsUIEvents = () =>{
+
+        $("#themeDarkOption").on("click", ()=>SetTheme("Dark"))
+        $("#themeLightOption").on("click", ()=>SetTheme("Light"))
+
+        $("#subMenuDisplayShownOption").on("click", ()=>SetSubMenuShown(true))
+        $("#subMenuDisplayHiddenOption").on("click", ()=>SetSubMenuShown(false))
+
+        $("#exportSaveToClipboard").on("click", ()=>ExportSaveToClipboard())
+        $("#importSave").on("click", ()=>{ ShowDialogBox("Import Save",
+             "Importing a save will overwrite your current save. <br> <input id='dialogBoxTextarea' placeholder='paste your save here'> </input>", "Warning", ImportSave)})
+        $("#hardReset").on("click", ()=>{ ShowDialogBox("Hard Reset", "Are you sure you want to hard reset? This action is irreversible.", "Danger", HardReset)})
+    }
+    //#endregion
+    //#region Set theme
+    const SetTheme = (newTheme)=>{
+        $(newTheme=="Dark" ? "#themeLightOption" : "#themeDarkOption").removeClass("selectedOption")
+        $("#container").removeClass(`theme-${newTheme == "Light" ? "dark" : "light"}`)
+        player.options.ui.theme=newTheme
+        $(newTheme=="Dark" ? "#themeDarkOption" : "#themeLightOption").addClass("selectedOption")
+        $("#container").addClass(`theme-${newTheme == "Light" ? "light" : "dark"}`)
+        Save()
+    }
+    //#endregion
+    //#region Set subMenuShown
+    const SetSubMenuShown = (newDisplay)=>{
+        player.options.ui.subMenuShown=newDisplay
+        GoToSettings()
+        Save()
+    }
+    //#endregion
+    //#region Export Save to clipboard
+    const ExportSaveToClipboard = ()=>{
+        let save= localStorage.getItem("replicantiIncSave")
+        navigator.clipboard.writeText(save);
+        ShowNotification("Success", "Save exported to clipboard", "Success")
+    }
+    //#endregion
+    //#region Information nav
+    $("#information").on("click", ()=>{
+        mainMenuIndex=1
+        GoToInformation()
+    })
+
+    const GoToInformation = () =>{
+        switch(subMenuIndexes[1]){
+            case 0:
+                GoToMainInformation();
+                break;
+            case 1:
+                GoToChangelogInformation();
+                break;
+            default:
+                console.log("Settings sub navigation broke")
+                break;
+        }
+    }
+    //#endregion
+    //#region GoToMainInformation
+    const GoToMainInformation = () =>{
+        view.html(`
+            <div id="subMenuInView" ${player.options.ui.subMenuShown==false ? 'class="subMenuHidden"' : ""}>
+                <div class="subMenuItem selectedSubMenuItem">
+                    Main
+                </div>
+                <div id="changelogSubMenuItem" class="subMenuItem interactable">
+                    Changelog
+                </div>
+            </div>
+            <div class="mainView">
+                <div class="gameRelatedMainInfo">
+                    <div class="informationGameTitle">
+                        mini-Infinity-games <br> Board of Inflation
+                    </div>
+                    <div class="gameVersion">
+                        V1: Release
+                    </div>
+                    <div class="gameAuthor">
+                        Made by SzaeZal
+                    </div>
+                </div>
+            </div>
+        `)
+
+        AddMainInformationUIEvents()
+    }
+    //#endregion
+    //#region AddMainInformationUIEvents
+    const AddMainInformationUIEvents=()=>{
+        if(player.options.ui.subMenuShown==true){
+            $("#changelogSubMenuItem").on("click", ()=>{
+                subMenuIndexes[mainMenuIndex]=1
+                GoToChangelogInformation()
+            })
+        }
+        else{
+            $("#changelogSubMenuItem").removeClass("interactable")
+        }
+    }
+    //#endregion
+    //#region GoToChangelogInformation
+    const GoToChangelogInformation = () =>{
+        view.html(`
+            <div id="subMenuInView" ${player.options.ui.subMenuShown==false ? 'class="subMenuHidden"' : ""}>
+                <div class="subMenuItem interactable" id="mainInformationSubMenuItem">
+                    Main
+                </div>
+                <div class="subMenuItem selectedSubMenuItem">
+                    Changelog
+                </div>
+            </div>
+            <div class="mainView">
+                <div class="changelog">
+                    <div class="version">
+                        <div class="changelogVersionName">
+                            V1: Release
+                        </div>
+                        <div class="changelogChanges">
+                            <ul>
+                                <li>Initial release</li>
+                                <li>Added board game</li>
+                                <li>Added settings</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>    
+        `)
+        AddChangelogInformationUIEvents()
+    }
+    //#endregion
+    //#region AddChangelogInformationUIEvents
+    const AddChangelogInformationUIEvents=()=>{
+        if(player.options.ui.subMenuShown==true){
+            $("#mainInformationSubMenuItem").on("click", ()=>{
+                subMenuIndexes[mainMenuIndex]=0
+                GoToMainInformation()
+            })
+        }
+        else{
+            $("#mainInformationSubMenuItem").removeClass("interactable")
+        }
+    }
+    //#endregion
+    //#region game menu
+    $("#gameMenuItem").on("click", ()=>{
+        mainMenuIndex=2
+        GoToGameMenu()
+    })
+
+    const GoToGameMenu=()=>{
+        view.html(`
+            <div id="subMenuInView" ${player.options.ui.subMenuShown==false ? 'class="subMenuHidden"' : ""}>
+                <div class="subMenuItem selectedSubMenuItem">
+                    Main
+                </div>
+            </div>
+            <div class="mainView">
+                
+            </div>  
+        `)
+        AddGameMenuUIEvents()  
+    }
+    //#endregion
+    //#region  AddGameMenuUIEvents
+    const AddGameMenuUIEvents = ()=>{
+        $("#easyDifficulty").on("click", ()=>{
+            ShowDifficultyStats("Easy")
+        })
+        $("#mediumDifficulty").on("click", ()=>{
+            ShowDifficultyStats("Medium")
+        })
+        $("#hardDifficulty").on("click", ()=>{
+            ShowDifficultyStats("Hard")
+        })
+        $("#customDifficulty").on("click", ()=>{
+            ShowCustomDifficultySetup()
+        })
+    }
+    //#endregion
+    //#region  saving and loading 
+    const Save = () => {
+        const playerParsedToJson = JSON.stringify(player);
+        let jwt = CreatePartialJWT(playerParsedToJson);
+        localStorage.setItem("boardOfInflationSave", jwt);
+    };
+
+    const CreatePartialJWT = (payloadInJson) => {
+        let payloadInBase64 = btoa(payloadInJson);
+        let signatureInBase64 = btoa(JSON.stringify("LeastObviousSignature"));
+        let jwt = payloadInBase64 + "." + signatureInBase64;
+        return jwt;
+    };
+
+    const DecodePartialJwt = () => {
+        let jwt = localStorage.getItem("boardOfInflationSave");
+        if (jwt != null) {
+            let [payload, signature] = jwt.split(".");
+            if (JSON.parse(atob(signature)) != "LeastObviousSignature") {
+                alert("Ey man how about you don't alter the localstorage aight?");
+                return null;
+            }
+            return atob(payload);
+        }
+        return null;
+    };
+    
+    const Load = () => {
+        let playerSaveJson = DecodePartialJwt();
+        try {
+            let playerSaveParsed = JSON.parse(playerSaveJson);
+            player = Object.assign({}, player, playerSaveParsed);
+
+            CheckForMissingData();
+
+            SetTheme(player.options.ui.theme);
+        
+        }
+        catch (e) {
+            console.log(e);
+        }
+    };
+    
+    const CheckForMissingData = () => {
+
+    };
+
+    const HardReset = () => {
+        localStorage.removeItem("boardOfInflationSave");
+        location.reload();
+    };
+    
+    //#endregion
+    //#region Import save
+    const ImportSave = ()=>{
+        let saveText=$("#dialogBoxTextarea").val()
+        localStorage.setItem("boardOfInflationSave", saveText)
+        location.reload()
+    }
+
+    const ImportSaveFromText= (text)=>{
+        localStorage.setItem("boardOfInflationSave", text)
+        location.reload()
+    }
+    //#endregion
+    mainMenuCallbacks=[GoToSettings, GoToInformation, GoToGameMenu] 
+    AddGameMenuUIEvents()
+    Load()
+
+    if(window.innerWidth<500){
+        ShowDialogBox("Warning", "This game is not optimized for small screens. <br> Please use a device with a larger screen for the best experience. <br> or use landscape orientation", "Warning")
+    }
+    //setTimeout(()=>{debugger;} , 15000)
+})
