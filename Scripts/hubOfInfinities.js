83d38e87be7aa8ab3392443ef0e31f1065fd8b12
SzaeZal <Sza3Zal@gmail.com>
2025-11-20 14:16:06 +0100

    added hubOfInfinities general UI stuff renamed infinite mobs to its actual name

---
+++ Scripts/hubOfInfinities.js
@@ -0,0 +1,418 @@
+$(()=>{
+    $("#goBackToHub").on("click", ()=>{
+        window.location.href="../index.html"
+    })
+    //#region player
+    let player={
+        options:{
+            ui:{
+                theme: "Dark",
+                subMenuShown: true,
+            }
+        }
+    }
+    //#endregion
+    //#region sidebar open-close
+    let sidebar = $("#sidebar")
+    let isSidebarOpen=true
+    $("#close-open").on("click", ()=>{
+        if(isSidebarOpen){
+            sidebar.removeClass("openSidebar")
+            sidebar.addClass("closeSidebar")
+            $("#close-open").html(`
+                <svg 
+                    xmlns="http://www.w3.org/2000/svg" 
+                    height="49px" 
+                    viewBox="0 -960 960 960" 
+                    width="49px" 
+                    fill="#888">
+                        <path d="M504-480 320-664l56-56 240 240-240 240-56-56 184-184Z"/>
+                </svg>
+            `)
+        }
+        else{
+            sidebar.removeClass("closeSidebar")
+            sidebar.addClass("openSidebar")
+            $("#close-open").html(`
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="49px"
+                    viewBox="0 -960 960 960"
+                    width="49px"
+                    fill="#888">
+                        <path d="M560-240 320-480l240-240 56 56-184 184 184 184-56 56Z" />
+                </svg>
+            `)
+        }
+        isSidebarOpen=!isSidebarOpen
+    })
+    //#endregion
+    //#region notification
+    let notificationIds=[0]
+    let notificationHiders=[]
+
+    const ShowNotification=(notificationTitle, notificationText, notificationType)=>{
+        let currentId=notificationIds[notificationIds.length-1]
+        $("#notifications").append(`
+            <div class="notification notification${notificationType} interactable" id="notification${currentId}">
+                ${  notificationTitle!=""
+                    ? `
+                        <h5>${notificationTitle}</h5>
+                        <hr>
+                    ` : ""
+                }
+                ${notificationText}
+            </div>    
+        `)
+
+        
+        notificationHiders.push(setTimeout(()=>{
+            $(`#notification${currentId}`).addClass("hiddenNotification")
+        }, 4000))
+
+        $("#notification"+currentId).on("click", ()=>{
+            clearTimeout(notificationHiders[currentId])
+            $(`#notification${currentId}`).addClass("hiddenNotification")
+        })
+
+        notificationIds.push(notificationIds.length)
+    }
+    //#endregion
+    //#region dialog box
+    const ShowDialogBox=(dialogTitle, dialogText, dialogType, callbackYes, callbackNo)=>{
+        $("#dialogBox").html(`
+            <div class="dialogBoxContainer dialogBox${dialogType}">
+                <div class="dialogBoxHeader">
+                    ${dialogTitle}
+                    <div id="closeDialogBox" class="closeDialogBox interactable">&#10005;</div>
+                </div>
+                <div class="dialogBoxContent">
+                    ${dialogText}
+                </div>
+                <div class="dialogBoxButtons">
+                    ${callbackYes==undefined 
+                        ? `<div id="dialogBoxCancel" class="dialogBoxButton dialogBoxButton${dialogType} interactable">I understand</div>`
+                        : `
+                            <div id="dialogBoxCancel" class="dialogBoxButton dialogBoxButton${dialogType} interactable">Cancel</div>
+                            <div id="dialogBoxYes" class="dialogBoxButton dialogBoxButton${dialogType} interactable">Yes</div>
+                        `
+                    }
+                </div>
+            </div>
+        `)
+        $("#dialogBox").removeClass("hiddenDialogBox")
+        AddDialogBoxEvents(callbackYes, callbackNo)
+    }
+
+    const AddDialogBoxEvents=(callbackYes, callbackNo)=>{
+        $("#closeDialogBox").on("click", ()=>{
+            $("#dialogBox").addClass("hiddenDialogBox")
+        })
+        $("#dialogBoxCancel").on("click", ()=>{
+            $("#dialogBox").addClass("hiddenDialogBox")
+            if(callbackNo) callbackNo()
+        })
+        if(callbackYes==undefined) return
+        $("#dialogBoxYes").on("click", ()=>{
+            callbackYes()
+            $("#dialogBox").addClass("hiddenDialogBox")
+        })
+    }
+    //#endregion
+    //#region menu navigation
+    const view = $("#view")
+    let mainMenuIndex=2
+    let subMenuIndexes=[0, 0, 0]
+    let subMenuLimits=[0, 1, 0]
+    $(document).keydown((e)=>{     
+        if(e.originalEvent.code == "KeyS" || e.originalEvent.code == "ArrowDown"){
+            mainMenuIndex= mainMenuIndex == subMenuLimits.length-1 ? 0 : mainMenuIndex+1
+            mainMenuCallbacks[mainMenuIndex]()
+        }
+        else if(e.originalEvent.code == "KeyW" || e.originalEvent.code == "ArrowUp"){
+            mainMenuIndex= mainMenuIndex == 0 ? subMenuLimits.length-1 : mainMenuIndex-1
+            mainMenuCallbacks[mainMenuIndex]()
+        }
+        else if(e.originalEvent.code == "KeyA" || e.originalEvent.code == "ArrowLeft"){
+            subMenuIndexes[mainMenuIndex] = subMenuIndexes[mainMenuIndex] == 0 ? subMenuLimits[mainMenuIndex] : subMenuIndexes[mainMenuIndex]-1
+            mainMenuCallbacks[mainMenuIndex]()
+        }
+        else if(e.originalEvent.code == "KeyD" || e.originalEvent.code == "ArrowRight"){
+            subMenuIndexes[mainMenuIndex] = subMenuIndexes[mainMenuIndex] == subMenuLimits[mainMenuIndex] ? 0 : subMenuIndexes[mainMenuIndex]+1
+            mainMenuCallbacks[mainMenuIndex]()
+        }
+    })
+
+    //#endregion
+    //#region Settings
+    $("#options").on("click", ()=>{
+        mainMenuIndex=0
+        GoToSettings()
+    })
+
+    const GoToSettings = () =>{
+        view.html(`
+            <div id="subMenuInView" ${player.options.ui.subMenuShown==false ? 'class="subMenuHidden"' : ''}>
+                <div class="subMenuItem selectedSubMenuItem">
+                    Settings
+                </div>
+            </div>
+            <div class="mainView">
+                <div class="settings">
+                    <div class="setting">
+                        <div class="settingTitle">
+                            Theme
+                        </div>
+                        <div class="options">
+                            <div id="themeDarkOption" class="option interactable ${player.options.ui.theme=="Dark" ? "selectedOption" : ""}">
+                                Dark
+                            </div>
+                            <div id="themeLightOption" class="option interactable ${player.options.ui.theme=="Light" ? "selectedOption" : ""}">
+                                Light
+                            </div>
+                        </div>
+                    </div>
+                    <div class="setting">
+                        <div class="settingTitle">
+                            Sub menu display
+                        </div>
+                        <div class="options">
+                            <div id="subMenuDisplayShownOption" class="option interactable ${player.options.ui.subMenuShown==true ? "selectedOption" : ""}">
+                                Shown
+                            </div>
+                            <div id="subMenuDisplayHiddenOption" class="option interactable ${player.options.ui.subMenuShown==false ? "selectedOption" : ""}">
+                                Hidden
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>    
+        `)
+        AddSettingsUIEvents()
+    }
+    //#endregion
+    //#region Settings Events
+    const AddSettingsUIEvents = () =>{
+
+        $("#themeDarkOption").on("click", ()=>SetTheme("Dark"))
+        $("#themeLightOption").on("click", ()=>SetTheme("Light"))
+
+        $("#subMenuDisplayShownOption").on("click", ()=>SetSubMenuShown(true))
+        $("#subMenuDisplayHiddenOption").on("click", ()=>SetSubMenuShown(false))
+
+    }
+    //#endregion
+    //#region Set theme
+    const SetTheme = (newTheme)=>{
+        $(newTheme=="Dark" ? "#themeLightOption" : "#themeDarkOption").removeClass("selectedOption")
+        $("#container").removeClass(`theme-${newTheme == "Light" ? "dark" : "light"}`)
+        player.options.ui.theme=newTheme
+        $(newTheme=="Dark" ? "#themeDarkOption" : "#themeLightOption").addClass("selectedOption")
+        $("#container").addClass(`theme-${newTheme == "Light" ? "light" : "dark"}`)
+        Save()
+    }
+    //#endregion
+    //#region Set subMenuShown
+    const SetSubMenuShown = (newDisplay)=>{
+        player.options.ui.subMenuShown=newDisplay
+        GoToSettings()
+        Save()
+    }
+    //#endregion
+    //#region Information nav
+    $("#information").on("click", ()=>{
+        mainMenuIndex=1
+        GoToInformation()
+    })
+
+    const GoToInformation = () =>{
+        switch(subMenuIndexes[1]){
+            case 0:
+                GoToMainInformation();
+                break;
+            case 1:
+                GoToChangelogInformation();
+                break;
+            default:
+                console.log("Settings sub navigation broke")
+                break;
+        }
+    }
+    //#endregion
+    //#region GoToMainInformation
+    const GoToMainInformation = () =>{
+        view.html(`
+            <div id="subMenuInView" ${player.options.ui.subMenuShown==false ? 'class="subMenuHidden"' : ""}>
+                <div class="subMenuItem selectedSubMenuItem">
+                    Main
+                </div>
+                <div id="changelogSubMenuItem" class="subMenuItem interactable">
+                    Changelog
+                </div>
+            </div>
+            <div class="mainView">
+                <div class="gameRelatedMainInfo">
+                    <div class="informationGameTitle">
+                        mini-Infinity-games
+                    </div>
+                    <div class="gameVersion">
+                        V0.9: The Experiments
+                    </div>
+                    <div class="gameAuthor">
+                        Made by SzaeZal
+                    </div>
+                </div>
+            </div>
+        `)
+
+        AddMainInformationUIEvents()
+    }
+    //#endregion
+    //#region AddMainInformationUIEvents
+    const AddMainInformationUIEvents=()=>{
+        if(player.options.ui.subMenuShown==true){
+            $("#changelogSubMenuItem").on("click", ()=>{
+                subMenuIndexes[mainMenuIndex]=1
+                GoToChangelogInformation()
+            })
+        }
+        else{
+            $("#changelogSubMenuItem").removeClass("interactable")
+        }
+    }
+    //#endregion
+    //#region GoToChangelogInformation
+    const GoToChangelogInformation = () =>{
+        view.html(`
+            <div id="subMenuInView" ${player.options.ui.subMenuShown==false ? 'class="subMenuHidden"' : ""}>
+                <div class="subMenuItem interactable" id="mainInformationSubMenuItem">
+                    Main
+                </div>
+                <div class="subMenuItem selectedSubMenuItem">
+                    Changelog
+                </div>
+            </div>
+            <div class="mainView">
+                <div class="changelog">
+                    <div class="version">
+                        <div class="changelogVersionName">
+                            V0.9: The Experiments
+                        </div>
+                        <div class="changelogChanges">
+                            <ul>
+                                <li>Initial release</li>
+                                <li>Added Replicanti Incremental</li>
+                                <li>Added Cards of Infinity</li>
+                                <li>Added Board of Inflation</li>
+                                <li>Added Infinite Gates</li>
+                                <li>Added ...</li>  
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>    
+        `)
+        AddChangelogInformationUIEvents()
+    }
+    //#endregion
+    //#region AddChangelogInformationUIEvents
+    const AddChangelogInformationUIEvents=()=>{
+        if(player.options.ui.subMenuShown==true){
+            $("#mainInformationSubMenuItem").on("click", ()=>{
+                subMenuIndexes[mainMenuIndex]=0
+                GoToMainInformation()
+            })
+        }
+        else{
+            $("#mainInformationSubMenuItem").removeClass("interactable")
+        }
+    }
+    //#endregion
+    //#region games menu
+    $("#gameMenuItem").on("click", ()=>{
+        mainMenuIndex=2
+        GoToGamesMenu()
+    })
+
+    const GoToGamesMenu=()=>{
+        view.html(`
+            <div id="subMenuInView" ${player.options.ui.subMenuShown==false ? 'class="subMenuHidden"' : ""}>
+                <div class="subMenuItem selectedSubMenuItem">
+                    Main
+                </div>
+            </div>
+            <div class="mainView">
+                <nav>
+                    <a href="Views/boardOfInflation.html">
+                        Board of Inflation
+                    </a>
+                    <a href="Views/cardsOfInfinity.html">
+                        Cards of Infinity
+                    </a>
+                    <a href="Views/infiniteMobs.html">
+                        Infinite mobs
+                    </a>
+                    <a href="Views/replicantiIncremental.html">
+                        Replicanti Incremental
+                    </a>
+                    <a href="Views/infiniteGates.html">
+                        Infinite Gates
+                    </a>
+                </nav>
+            </div>  
+        `)
+        AddGamesMenuUIEvents()  
+    }
+    //#endregion
+    //#region  AddGamesMenuUIEvents
+    const AddGamesMenuUIEvents = ()=>{
+        
+    }
+    //#endregion
+    //#region  saving and loading 
+    const Save = () => {
+        const playerParsedToJson = JSON.stringify(player);
+        let jwt = CreatePartialJWT(playerParsedToJson);
+        localStorage.setItem("hubOfInfinitiesSave", jwt);
+    };
+
+    const CreatePartialJWT = (payloadInJson) => {
+        let payloadInBase64 = btoa(payloadInJson);
+        let signatureInBase64 = btoa(JSON.stringify("LeastObviousSignature"));
+        let jwt = payloadInBase64 + "." + signatureInBase64;
+        return jwt;
+    };
+
+    const DecodePartialJwt = () => {
+        let jwt = localStorage.getItem("hubOfInfinitiesSave");
+        if (jwt != null) {
+            let [payload, signature] = jwt.split(".");
+            if (JSON.parse(atob(signature)) != "LeastObviousSignature") {
+                alert("Ey man how about you don't alter the localstorage aight?");
+                return null;
+            }
+            return atob(payload);
+        }
+        return null;
+    };
+    
+    const Load = () => {
+        let playerSaveJson = DecodePartialJwt();
+        try {
+            let playerSaveParsed = JSON.parse(playerSaveJson);
+            player = Object.assign({}, player, playerSaveParsed);
+
+            SetTheme(player.options.ui.theme);
+        
+        }
+        catch (e) {
+            console.log(e);
+        }
+    };
+    
+    //#endregion
+    mainMenuCallbacks=[GoToSettings, GoToInformation, GoToGamesMenu] 
+    AddGamesMenuUIEvents()
+    Load()
+    //setTimeout(()=>{debugger;} , 15000)
+})
